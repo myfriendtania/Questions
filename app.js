38a145853f7829a5da2fe5253b78f1a205b8e268
myfriendtania <thetalentedleo2017@gmail.com>
2022-10-24 19:04:45 -0500

    problems with minus sign, project still complete

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 //using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question) {
+ const btn = question.querySelector('.question-btn');
+
+ btn.addEventListener('click', function () {
+
+question.forEach(function (item) {
+ if(item !== question) {
+    item.classList.remove("show-text");
+ }
+});
+
+    question.classList.toggle("show-text");
+ });
+});
+
+
 // traversing the dom
 
 
@@ -6,6 +25,8 @@ const btns = document.querySelectorAll(".question-btn");
 
 btns.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
-        console.log(e.currentTarget.parentElement);
+        const question = e.currentTarget.parentElement.
+        parentElement;
+        question.classList.toggle("show-text");
     });
 });
